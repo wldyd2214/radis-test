c389fbcad7fd8e245137a9fc404b95cf93568940
박지용 <park@bagjiyong-ui-MacBookPro.local>
2021-09-03 09:33:39 +0900

    controller add commit

--- example/src/main/java/com/example/radis/ExampleApplication.java
+++ example/src/main/java/com/example/radis/ExampleApplication.java
@@ -10,13 +10,6 @@ public class ExampleApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ExampleApplication.class, args);
-		
-		System.out.println("연결 시도");
-		Jedis jedis = new Jedis("radis-test.0wchch.ng.0001.apn2.cache.amazonaws.com", 6379);
-		System.out.println("연결 완료");
-        jedis.set("users:jypark", "park ju yong|seoul|010-1234-1234");
-        String value = jedis.get("users:jypark");
-        System.out.println(value);
 	}
 
 }
